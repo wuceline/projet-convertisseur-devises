99a8833e4a9b47170681a4bdd17a72ad22ee02e4
wucéline <wuwuceline@gmail.com>
2021-09-17 11:16:31 +0200

    correction

--- src/components/Currencies/index.js
+++ src/components/Currencies/index.js
@@ -2,19 +2,23 @@ import PropTypes from 'prop-types';
 import './currencies.scss';
 
 const Currencies = ({ currencies }) => (
-  <ol className="currencies">
-    <p className="currencies-title">Currencies</p>
-    {currencies.map((currency) => (
-      <li className="currency">{currency.name}</li>
-    ))}
-  </ol>
+  <div className="currencies">
+    <div className="currencies-title">Currencies</div>
+    <ol>
+      {currencies.map((currency) => (
+        <li className="currency" key={currency.name}>
+          {currency.name}
+        </li>
+      ))}
+    </ol>
+  </div>
 );
 
 Currencies.propTypes = {
   currencies: PropTypes.arrayOf(
     PropTypes.shape({
       name: PropTypes.string.isRequired,
-      rate: PropTypes.number.isRequired,
+      // rate: PropTypes.number.isRequired,
     }).isRequired,
   ).isRequired,
 };
